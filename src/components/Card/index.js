e1af7bf72fd0b1aa1506e64d4d26b13c297bacf5
Rafael CNC <rafael.cechinato@pucpr.edu.br>
2021-04-25 17:01:26 -0300

    melhorias

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import { View } from 'react-native';
-import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import { View, TouchableWithoutFeedback, Keyboard } from 'react-native';
 import styles from '../../assets/css/styles';
 
 export default function Card(props) {
 
     return (
-
-        <View style={styles.viewContainer}>
-            <View style={styles.card}>
-                {props.children}
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+            <View style={styles.viewContainer}>
+                <View style={styles.card}>
+                    {props.children}
+                </View>
             </View>
+        </TouchableWithoutFeedback>
 
-        </View>
     );
 
 }
